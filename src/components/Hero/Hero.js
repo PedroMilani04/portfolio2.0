23ca2b4bd973e867567325020eb727566c33c9a1
Pedro Henrique Milani Vagula <pedrohenriquehmv@gmail.com>
2024-01-09 18:05:46 -0300

    parallax 1

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,11 +1,27 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Hero.css';
 import Aos from 'aos';
 import 'aos/dist/aos.css';
 
 function Hero() {
+    const [scrollPosition, setScrollPosition] = useState(0);
+
     useEffect(() => {
         Aos.init({ duration: 1000 });
+    }, []);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setScrollPosition(window.scrollY);
+        };
+
+        // Add a scroll event listener
+        window.addEventListener('scroll', handleScroll);
+
+        // Remove the event listener on component unmount
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
     }, []);
 
     return (
@@ -14,7 +30,7 @@ function Hero() {
                 <h3 data-aos="fade-left" data-aos-duration="1000">Hello there! My name is</h3>
                 {/*                 <h1 data-aos="fade-right" data-aos-duration="2700" style={{ backgroundPositionY: `${scrollPosition}px` }}>PEDRO</h1>
  */}
-                <div className='maintext'>
+                <div className='maintext' style={{ backgroundPositionY: `${scrollPosition}px` }}>
                     <div className='spans'>
                         <span className='1' data-aos="fade-up" data-aos-duration="500" data-aos-delay="400">P</span>
                         <span className='2' data-aos="fade-up" data-aos-duration="500" data-aos-delay="900">E</span>
