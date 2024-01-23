1ce852eefe25c8147cd335efac94e5e39935442c
Pedro Henrique Milani Vagula <pedrohenriquehmv@gmail.com>
2024-01-23 14:34:34 -0300

    menu

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,23 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 function Navbar() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
-        <nav className="navbar">
-            <ul className="navbar-list">
+        <nav className={`navbar ${isMenuOpen ? 'open' : ''}`}>
+            <div className='nav-title'>
+                P. Milani
+            </div>
+            <div className={`navbar-toggle`} onClick={toggleMenu}>
+                <FontAwesomeIcon icon={faBars} className='bars'/>
+            </div>
+            <ul className={`navbar-list ${isMenuOpen ? 'open' : ''}`}>
                 <li className="navbar-item">
                     <a href="#" className="navbar-link">Home</a>
                 </li>
