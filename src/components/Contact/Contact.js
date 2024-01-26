21fae4311405650383099dd61926782d6cbaec1a
Pedro Henrique Milani Vagula <pedrohenriquehmv@gmail.com>
2024-01-26 19:13:03 -0300

    working links

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -9,6 +9,22 @@ function Contact() {
         Aos.init({ duration: 1000 });
     }, []);
 
+    const openInstagram = () => {
+        window.open('https://www.instagram.com/phvmilani?igsh=Mzh3bG13NTh0bnU=', '_blank');
+    };
+
+    const openLinkedin = () => {
+        window.open('https://www.linkedin.com/in/pedromilanidev/', '_blank');
+    };
+
+    const openGitHub = () => {
+        window.open('https://github.com/PedroMilani04', '_blank');
+    };
+
+    const openGmail = () => {
+        window.open('mailto:milanipedrovagula@gmail.com', '_blank');
+    };
+
     return (
         <div className="Contact">
             <h1 className='titlecontact'  data-aos="fade-up">Contact</h1>
@@ -17,7 +33,7 @@ function Contact() {
                     </div>
             <div className='circle'>
             <div class="up">
-                <button class="card1">
+                <button class="card1" onClick={openInstagram}>
                     <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 256 256" width="30px" height="30px" fillRule="nonzero" className="instagram">
                         <g fillRule="nonzero" stroke="none" strokeWidth="1" strokeLinecap="butt" strokeLinejoin="miter" strokeMiterlimit="10" strokeDasharray="" strokeDashoffset="0" fontFamily="none" fontWeight="none" fontSize="none" textAnchor="none" style={{ mixBlendMode: 'normal' }}>
                             <g transform="scale(8,8)">
@@ -26,15 +42,15 @@ function Contact() {
                         </g>
                     </svg>
                 </button>
-                <button class="card2">
+                <button class="card2" onClick={openGmail}>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="50px" height="50px" class="twitter"><path d="M 16.003906 14.0625 L 16.003906 18.265625 L 21.992188 18.265625 C 21.210938 20.8125 19.082031 22.636719 16.003906 22.636719 C 12.339844 22.636719 9.367188 19.664063 9.367188 16 C 9.367188 12.335938 12.335938 9.363281 16.003906 9.363281 C 17.652344 9.363281 19.15625 9.96875 20.316406 10.964844 L 23.410156 7.867188 C 21.457031 6.085938 18.855469 5 16.003906 5 C 9.925781 5 5 9.925781 5 16 C 5 22.074219 9.925781 27 16.003906 27 C 25.238281 27 27.277344 18.363281 26.371094 14.078125 Z"></path></svg>
                 </button>
             </div>
             <div class="down">
-                <button class="card3">
+                <button class="card3" onClick={openGitHub}>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30" width="30px" height="30px" class="github">    <path d="M15,3C8.373,3,3,8.373,3,15c0,5.623,3.872,10.328,9.092,11.63C12.036,26.468,12,26.28,12,26.047v-2.051 c-0.487,0-1.303,0-1.508,0c-0.821,0-1.551-0.353-1.905-1.009c-0.393-0.729-0.461-1.844-1.435-2.526 c-0.289-0.227-0.069-0.486,0.264-0.451c0.615,0.174,1.125,0.596,1.605,1.222c0.478,0.627,0.703,0.769,1.596,0.769 c0.433,0,1.081-0.025,1.691-0.121c0.328-0.833,0.895-1.6,1.588-1.962c-3.996-0.411-5.903-2.399-5.903-5.098 c0-1.162,0.495-2.286,1.336-3.233C9.053,10.647,8.706,8.73,9.435,8c1.798,0,2.885,1.166,3.146,1.481C13.477,9.174,14.461,9,15.495,9 c1.036,0,2.024,0.174,2.922,0.483C18.675,9.17,19.763,8,21.565,8c0.732,0.731,0.381,2.656,0.102,3.594 c0.836,0.945,1.328,2.066,1.328,3.226c0,2.697-1.904,4.684-5.894,5.097C18.199,20.49,19,22.1,19,23.313v2.734 c0,0.104-0.023,0.179-0.035,0.268C23.641,24.676,27,20.236,27,15C27,8.373,21.627,3,15,3z"></path></svg>
                 </button>
-                <button class="card4">
+                <button class="card4" onClick={openLinkedin}>
                     <svg height="80px" width="80px" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg" class="discord">
                     <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z"/>
                         </svg>
